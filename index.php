0a84d9e216862a953d4895e61a8bca66ba908469
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-11-01 08:32:33 +0100

    web site Architecture

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>GUI1</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" media="all" href="src/style/style.css" />
+</head>
+
+<body>
+
+    <?php
+    include('src/php/nav.php');
+    ?>
+</body>
+
+</html>
