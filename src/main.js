cd1aedbfc305513737c50db503832e2eaff96a78
Anthony Bouillant <bouillant.anthony@gmail.com>
2021-11-06 00:27:35 +0100

    Add event to calendar

--- src/main.js
+++ src/main.js
@@ -8,17 +8,74 @@ document.addEventListener('DOMContentLoaded', function () {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin, timeGridPlugin],
+    initialDate: '2021-11-05',
+    initialView: 'timeGridDay',
+    slotMinTime: '07:00:00',
+    slotMaxTime: '22:00:00',
+    editable: true,
     locales: [frLocale],
     locale: 'fr',
-    plugins: [dayGridPlugin, timeGridPlugin],
-    initialView: 'timeGridDay',
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'timeGridDay,timeGridWeek'
     },
-    slotMinTime: '07:00:00',
-    slotMaxTime: '22:00:00'
+    events: [
+      {
+        title: 'GPR1',
+        start: '2021-11-05T08:00:00',
+        end: '2021-11-05T09:35:00',
+        extendedProps: {
+          teacher: 'Glassey Nicolas',
+          room: 'SC-C332'
+        },
+      },
+      {
+        title: 'MAW 1.1 - E-Commerce',
+        start: '2021-11-05T09:50:00',
+        end: '2021-11-05T12:15:00',
+        extendedProps: {
+          teacher: 'Glassey Nicolas',
+          room: 'SC-C332'
+        },
+      },
+      {
+        title: 'CLD1',
+        start: '2021-11-05T13:30:00',
+        end: '2021-11-05T15:05:00',
+        extendedProps: {
+          teacher: 'Hurni Pascal',
+          room: 'SC-C332'
+        },
+      },
+      {
+        title: 'Rattrapages - Te - Retenues',
+        start: '2021-11-05T15:20:00',
+        end: '2021-11-05T16:55:00',
+        extendedProps: {
+          teacher: 'Rochat Claude, Zen-Ruffiner Xavier',
+          room: 'SC-C131'
+        },
+      }
+    ],
+    eventDidMount: function (info) {
+
+      const teacher = document.createElement('i');
+      teacher.textContent = info.event.extendedProps.teacher;
+      teacher.className = 'extendedProps'
+
+      const room = document.createElement('i');
+      room.textContent = info.event.extendedProps.room;
+      room.className = 'extendedProps'
+
+      const element = info.el.querySelector('.fc-event-title');
+
+      element.after(teacher);
+      teacher.after(room);
+      room.before(document.createElement('br'))
+
+    },
   });
 
   const slotLabelOption = {
